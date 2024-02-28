cfb0f79da7aa4b175b5fa9c35c406f21733c42b3
otto18 <otto@ceramic.global>
2024-02-28 13:44:27 -0500

    add Navbar back in

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 /** The root layout for the application. */
 export default function Root() {
   return (
     <>
-      {/*<Navbar />*/}
+      <Navbar />
       <main>
         <Outlet />
       </main>
